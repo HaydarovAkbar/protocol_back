c7bc8a5e2d9028ab5a0e963a294eca15c961f36d
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-05-23 13:31:50 +0500

    sd

--- src/app/models.py
+++ src/app/models.py
@@ -5,6 +5,38 @@ from utils.models import BaseModel
 from sport.models import SportType
 
 
+class WeightCategory(BaseModel):
+    title = models.CharField(max_length=255)
+    attr = models.CharField(max_length=255, null=True, blank=True)
+    sport_type = models.ForeignKey(SportType, on_delete=models.SET_NULL, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Weight Categories'
+        verbose_name = 'Weight Category'
+        db_table = 'weight_category'
+
+
+class AgeCategory(BaseModel):
+    title = models.CharField(max_length=255)
+    attr = models.CharField(max_length=255, null=True, blank=True)
+    sport_type = models.ForeignKey(SportType, on_delete=models.SET_NULL, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Age Categories'
+        verbose_name = 'Age Category'
+        db_table = 'age_category'
+
+
 class Competition(BaseModel):
     title = models.CharField(max_length=255)
     attr = models.CharField(max_length=255, null=True, blank=True)
